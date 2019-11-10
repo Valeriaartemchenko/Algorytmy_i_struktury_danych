ad9a3d49218420899d1fa6782f2dfaa427e8d3e4
Valeria Artemchemko <valeria.artemchenko@gmail.com>
2019-11-10 23:05:02 +0100

    добавила сортировку heap sort базовую

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-'use strict';
+//'use strict';
 var STEP = 3; // шаг изменения числа 
 
 // получаем число рандомно
@@ -200,9 +200,83 @@ var mergeSort = (arr) => {
         right = arr.slice(mid);
     return merge(mergeSort(left), mergeSort(right));
 };
-
+/*
 sortBtn.addEventListener('click', function(){
   var sortedArray = mergeSort(newarray);
   console.log(sortedArray);
 });
+*/
+/*--------------------------------Heap Sort ---------------------------*/
+function swap(array, firstItemIndex, lastItemInde) {
+  var tmp = array[firstItemIndex];
+  
+  // Swap first and last items in the array.
+  array[firstItemIndex] = array[lastItemInde];
+  array[lastItemInde] = tmp;
+}
 
+function heapify(heap, i, max) {
+  var index, leftChild, righChild;
+  
+  while(i < max) {
+    index = i;
+
+    leftChild = 2*i + 1;
+    righChild = leftChild + 1;
+
+    if (leftChild < max && heap[leftChild] > heap[index]) {
+      index = leftChild;
+    }
+
+    if (righChild < max && heap[righChild] > heap[index]) {
+      index = righChild;
+    }
+      
+    if (index == i) {
+      return;
+    }
+
+    swap(heap,i, index);
+    
+    i = index;
+  }
+}
+
+function buildMaxHeap(array) {
+  var i;
+  i = array.length / 2 - 1;
+  i = Math.floor(i);
+
+  // Build a max heap out of
+  // all array elements passed in.
+  while (i >= 0) {
+    heapify(array, i, array.length);
+    i -= 1;
+  }
+}
+
+
+function heapSort(array) {
+  // Build our max heap.
+  buildMaxHeap(array);
+
+  // Find last element.
+  lastElement = array.length - 1;
+
+  // Continue heap sorting until we have
+  // just one element left in the array.
+  while(lastElement > 0) {
+    swap(array, 0, lastElement);
+
+    heapify(array, 0, lastElement);
+
+    lastElement -= 1
+  }
+  return array;
+}
+
+
+sortBtn.addEventListener('click', function(){
+  var sortedArray = heapSort(newarray);
+  console.log(sortedArray);
+});
